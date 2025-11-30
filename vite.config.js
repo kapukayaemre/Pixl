fb8b2835e2ae89c875a527b5cdb6797af9aac6de
Emre Kapukaya <kapukayaemre@hotmail.com>
2025-11-30 21:40:56 +0300

    Laravel installed

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [tailwindcss()],
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
 });
