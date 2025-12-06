9e8014c3aa579ced63818976ea5ddb0cb6db3637
Emre Kapukaya <kapukayaemre@hotmail.com>
2025-12-06 22:22:28 +0300

    Post model, migration and factory created

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Profile extends Model
 {
@@ -21,5 +22,15 @@ class Profile extends Model
     public function belongsTo(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function topLevelPosts(): HasMany
+    {
+        return $this->hasMany(Post::class)->whereNull('parent_id');
     }
 }
