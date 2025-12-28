2787b0a0a1c4b87e6103e377328a11240560059b
Emre Kapukaya <kapukayaemre@hotmail.com>
2025-12-28 19:03:49 +0300

    Repost tests created

--- tests/Feature/PostBehaviorTest.php
+++ tests/Feature/PostBehaviorTest.php
@@ -17,7 +17,6 @@ test('allows a profile to publish a post', function () {
         ->and($post->repost_of_id)->toBeNull();
 });
 
-
 test('can reply to post', function () {
     $original = Post::factory()->create();
 
@@ -35,4 +34,36 @@ test('can have many replies', function () {
     expect($replies->first()->parent->is($original))->toBeTrue()
         ->and($original->replies)->toHaveCount(4)
         ->and($original->replies->contains($replies->first()))->toBeTrue();
+});
+
+test('create plain repost', function () {
+    $original = Post::factory()->create();
+
+    $repostProfile = Profile::factory()->create();
+    $repost = Post::repost($repostProfile, $original);
+
+    expect($repost->repostOf->is($original))->toBeTrue()
+        ->and($original->reposts)->toHaveCount(1)
+        ->and($repost->content)->toBeNull();
+});
+
+test('can have many reposts', function () {
+    $original = Post::factory()->create();
+    $reposts = Post::factory()->count(4)->repost($original)->create();
+
+    expect($reposts->first()->repostOf->is($original))->toBeTrue()
+        ->and($original->reposts)->toHaveCount(4)
+        ->and($original->reposts->contains($reposts->first()))->toBeTrue();
+});
+
+test('create quote repost', function () {
+    $content = 'quote content';
+    $original = Post::factory()->create();
+
+    $repostProfile = Profile::factory()->create();
+    $repost = Post::repost($repostProfile, $original, $content);
+
+    expect($repost->repostOf->is($original))->toBeTrue()
+        ->and($original->reposts)->toHaveCount(1)
+        ->and($repost->content)->toBe($content);
 });
