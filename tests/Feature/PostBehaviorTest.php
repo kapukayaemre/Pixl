b256259b98baaecd719f84e016a5ab54a68a01f7
Emre Kapukaya <kapukayaemre@hotmail.com>
2025-12-28 17:28:32 +0300

    Post reply tests created

--- tests/Feature/PostBehaviorTest.php
+++ tests/Feature/PostBehaviorTest.php
@@ -12,7 +12,27 @@ test('allows a profile to publish a post', function () {
     $post = Post::publish($profile, 'Content of the post');
 
     expect($post->exists)->toBeTrue()
-        ->and($post->profile()->is($profile))->toBeTrue()
+        ->and($post->profile->is($profile))->toBeTrue()
         ->and($post->parent_id)->toBeNull()
         ->and($post->repost_of_id)->toBeNull();
+});
+
+
+test('can reply to post', function () {
+    $original = Post::factory()->create();
+
+    $replier = Profile::factory()->create();
+    $reply = Post::reply($replier, $original, 'reply content');
+
+    expect($reply->parent->is($original))->toBeTrue()
+        ->and($original->replies)->toHaveCount(1);
+});
+
+test('can have many replies', function () {
+    $original = Post::factory()->create();
+    $replies = Post::factory()->count(4)->reply($original)->create();
+
+    expect($replies->first()->parent->is($original))->toBeTrue()
+        ->and($original->replies)->toHaveCount(4)
+        ->and($original->replies->contains($replies->first()))->toBeTrue();
 });
